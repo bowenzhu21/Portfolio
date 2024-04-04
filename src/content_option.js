ae3938a8f94f253a9b71d9276a98b6b74e42c96a
bowenzhu21 <160878510+bowenzhu21@users.noreply.github.com>
2024-04-04 11:04:00 -0400

    Initial Commit
    Start

---
+++ src/content_option.js
@@ -0,0 +1,169 @@
+const logotext = "BOWEN";
+const meta = {
+    title: "Bowen Zhu",
+    description: "I’m Bowen Zhu, Student & Software Developper _ Currently Working in Toronto",
+};
+
+const introdata = {
+    title: "I’m Bowen Zhu",
+    animated: {
+        first: "Software Developer",
+        second: "Student",
+        third: "Engineer",
+    },
+    description: "I'm a 17 year old Software Developer, Engineer & Student located in Toronto. I'm driven by innovation and hope to one day join the field of bionics",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Who Am I",
+    aboutme: "Hi, I'm Bowen 👋. I'm an innovation 💡 and engineering-driven ⚙️ leader. I'm a very active person both at work and at the gym. I'm a very creative 💡 person and I'm never shy to share my ideas",
+};
+const worktimeline = [{
+        jobtitle: "Schulich Leader Nominee",
+        where: "Canada",
+        date: "2024",
+    },
+    {
+        jobtitle: "1st Place (Model UN)",
+        where: "Univeristy of Toronto",
+        date: "2024",
+    },
+    {
+        jobtitle: "Loran Scholarship Shortlist",
+        where: "Canada",
+        date: "2023",
+    },
+    {
+        jobtitle: "Lloyd Auckland Invitational",
+        where: "Canada",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "JavaScript",
+        value: 75,
+    },
+    {
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Python - Machine Learning",
+        value: 70,
+    },
+    {
+        name: "SOLIDWORKS",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Design apps and websites for clients using React",
+    },
+    {
+        title: "Engineering",
+        description: "4 Internship experiences in mechanical, software and bioengineering",
+    },
+    {
+        title: "Tutoring",
+        description: "4+ years of experience tutoring grades k-12",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "To be lazy, is to be priviledged.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "bowenzhu66@gmail.com",
+    YOUR_FONE: "647-215-2868",
+    description: "I will get back to you as soon as I can!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    linkedin: "https://linkedin.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
